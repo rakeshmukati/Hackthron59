64c0c3c2a8d985cc131d28bad2a147918ab26ca4
VIJAY PATIDAR <vramotiya@gmail.com>
2020-08-10 19:57:46 +0530

    server issue fix db not found

--- index.js
+++ index.js
@@ -2,19 +2,22 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const MongoClient = require('mongodb').MongoClient;
 var md5 = require('md5');
+
+var db;
 const router = express.Router();
 const app = express();
-app.use(bodyParser)
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-const DATABASE_URL = "mongodb://u0xvnfqtghlzyzjll6tg:JQ1kWeJGxzjB8ufJauzi@b7mseqkz0wkghgb-mongodb.services.clever-cloud.com:27017/b7mseqkz0wkghgb"
-const DATABASE_NAME = "b7mseqkz0wkghgb"
+const DATABASE_URL = "mongodb://uq2br62itgkczdy3ld7o:3ax10rxF8qHi5uciSyuF@bkj8bp4plc6eccf-mongodb.services.clever-cloud.com:27017/bkj8bp4plc6eccf"
+const DATABASE_NAME = "bkj8bp4plc6eccf"
 
 router.post("/login", (req, res) => {
-
+    console.log(req.body)
 })
 
 router.post("/signup", (req, res) => {
-
+    console.log(req.body)
 })
 
 MongoClient.connect(DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true }, function(err, client) {
