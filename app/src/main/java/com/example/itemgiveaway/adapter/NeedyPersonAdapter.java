96471c759aeb5ea936d45350be616764908fb5e1
Rakesh <rakeshmukati2003@gmail.com>
2020-08-14 15:42:22 +0530

    Needy person information saved to data base and needy persons list  generated

---
+++ app/src/main/java/com/example/itemgiveaway/adapter/NeedyPersonAdapter.java
@@ -0,0 +1,62 @@
+package com.example.itemgiveaway.adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.widget.AppCompatImageView;
+import androidx.appcompat.widget.AppCompatTextView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.itemgiveaway.R;
+import com.example.itemgiveaway.model.NeedyItem;
+import com.example.itemgiveaway.utils.ImageUtils;
+
+import java.util.ArrayList;
+
+public class NeedyPersonAdapter extends RecyclerView.Adapter<NeedyPersonAdapter.MyViewHolder>  {
+    private final ArrayList<NeedyItem> items = new ArrayList<>();
+
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new MyViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.needy_list_item,parent,false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        final NeedyItem item = items.get(position);
+        holder.itemName.setText(item.getName());
+        holder.itemImage.setImageBitmap(new ImageUtils().stringToBitmap(item.getPicture()));
+        holder.itemcategory.setText(item.getCategoryId());
+        holder.itemNumber.setText(item.getPhone());
+        holder.address.setText(item.getAddress());
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+    static class MyViewHolder extends RecyclerView.ViewHolder{
+        AppCompatImageView itemImage;
+        AppCompatTextView itemName,itemNumber,itemcategory,address;
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            itemImage = itemView.findViewById(R.id.needy_image);
+            itemName = itemView.findViewById(R.id.needy_name);
+            itemNumber =itemView.findViewById(R.id.needy_number);
+            itemcategory=itemView.findViewById(R.id.need_categori);
+            address=itemView.findViewById(R.id.address);
+        }
+    }
+
+    public void setItems(ArrayList<NeedyItem> items) {
+        synchronized (this.items){
+            this.items.clear();
+            this.items.addAll(items);
+            notifyDataSetChanged();
+        }
+    }
+}
---
+++ app/src/main/java/com/example/itemgiveaway/controllers/NeedyController.java
@@ -0,0 +1,128 @@
+package com.example.itemgiveaway.controllers;
+
+import android.util.Log;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.example.itemgiveaway.MyRequestQueue;
+import com.example.itemgiveaway.model.Category;
+import com.example.itemgiveaway.model.NeedyItem;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+import java.util.ArrayList;
+import static com.example.itemgiveaway.MyRequestQueue.BASE_URL;
+
+public class NeedyController {
+    private static final String TAG = NeedyController.class.getSimpleName();
+    private static NeedyController controller = null;
+    private ArrayList<Category> categories = null;
+    private ArrayList<NeedyItem> items = null;
+
+    public NeedyController() {
+    }
+
+    public static synchronized NeedyController getInstance() {
+        if (controller == null) {
+            controller = new NeedyController();
+        }
+        return controller;
+    }
+    public void getCategories(final OnCategoriesListListener onCategoriesListListener) {
+        if (categories != null) {
+            onCategoriesListListener.onCategoriesListFetched(categories);
+        } else {
+            StringRequest stringRequest = new StringRequest(StringRequest.Method.GET,
+                    BASE_URL + "categories",
+                    new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+                            Log.d(TAG, "========================>" + response);
+                            try {
+                                Gson gson = new GsonBuilder().create();
+                                categories = new ArrayList<>();
+                                JsonArray jsonElements = gson.fromJson(response, JsonArray.class);
+                                for (int i = 0; i < jsonElements.size(); i++) {
+                                    categories.add(gson.fromJson(jsonElements.get(i), Category.class));
+                                }
+                                onCategoriesListListener.onCategoriesListFetched(categories);
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    },
+                    new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                        }
+                    }) {
+            };
+            MyRequestQueue.getInstance().addRequest(stringRequest);
+        }
+    }
+    public void getNeedyPersonList(final OnNeedyPersonListPreparesListener onNeedyPersonListPreparesListener) {
+        StringRequest stringRequest = new StringRequest(StringRequest.Method.GET,
+                BASE_URL + "needyPersons",
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Log.d(TAG, response);
+                        try {
+                            Gson gson = new GsonBuilder().create();
+                            items = new ArrayList<>();
+                            JsonArray jsonElements = gson.fromJson(response, JsonArray.class);
+                            for (int i = 0; i < jsonElements.size(); i++) {
+                                items.add(gson.fromJson(jsonElements.get(i), NeedyItem.class));
+                            }
+                            onNeedyPersonListPreparesListener.onNeedyItemListPrepared(items);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                    }
+                }) {
+        };
+        MyRequestQueue.getInstance().addRequest(stringRequest);
+    }
+    public void addNeedyPerson(final NeedyItem item) {
+        StringRequest stringRequest = new StringRequest(StringRequest.Method.PUT,
+                BASE_URL + "needypersons",
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Log.d(TAG, "================================>" + response);
+                        items.add(item);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d(TAG, "================================>" + error);
+                    }
+                }) {
+            @Override
+            public byte[] getBody() {
+                return new GsonBuilder().create().toJson(item).getBytes();
+            }
+
+            @Override
+            public String getBodyContentType() {
+                return "application/json";
+            }
+
+        };
+
+        MyRequestQueue.getInstance().addRequest(stringRequest);
+    }
+    public interface OnCategoriesListListener {
+        void onCategoriesListFetched(ArrayList<Category> categories);
+    }
+
+    public interface OnNeedyPersonListPreparesListener {
+        void onNeedyItemListPrepared(ArrayList<NeedyItem> items);
+    }
+}
